91a79be8f902d78141bdc22cb76e80022d5580ce
Tin Tran <tin.tran@split.io>
2023-03-29 23:11:28 -0700

    formatting

--- python_admin_api_tool/admin_api_tool.py
+++ python_admin_api_tool/admin_api_tool.py
@@ -281,12 +281,13 @@ def search_environments():
                 print(f"Environment(s) found with name: {env_name}")
                 print(f"Showing all environments of the same name across all workspaces:")
                 for env in found_envs:
-                    print("-------------------")
+                    print("-------------------------------------------")
                     pprint.pprint(env)
 
                 see_split_definitions = input("Do you want to see all the split definitions in this environment? (yes/no): ")
                 if see_split_definitions.lower() == "yes":
                     for env in found_envs:
+                        print("-------------------------------------------")
                         definitions = get_split_definitions(env["ID"], env["Workspace ID"])
                         print(f"Split definitions for environment {env_name} in workspace {env['Workspace Name']}:")
                         pprint.pprint(definitions)
