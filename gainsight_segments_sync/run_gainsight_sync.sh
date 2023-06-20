38e79dc172f7389e70abe79e3d125a98f8f18184
Tin Tran <tin.tran@split.io>
2023-06-20 14:56:13 -0700

    update runscript

--- gainsight_segments_sync/run_gainsight_sync.sh
+++ gainsight_segments_sync/run_gainsight_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /home/ec2-user/gainsight_sync/split_support_tools/gainsight_segments_sync
-python3 gs_sg_sync.py > gainsight_sync_output.txt 2>&1
+python3 gs_sg_sync.py >> gainsight_sync_output.txt 2>&1
